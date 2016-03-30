5a7ee1ff630c43c5fbae88fa5dccf29fa17fa31f
Jim Smith <zendagiwaqi@gmail.com>
2016-03-30 15:52:07 +0430

    commit

---
+++ www/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready( function() {
+	$("div[data-image]").each(function() {
+		$(this).css( "background-image", "url(" + $(this).data("image") + ")" );
+	});
+	
+	$( document ).on( "swiperight", function() {
+		nextPage();
+	});
+
+	$( document ).on( "swipeleft", function() {
+		previousPage();
+	});
+	
+	function nextPage() {
+		var nextPage = $( ":mobile-pagecontainer" ).pagecontainer("getActivePage").next("div[data-role='page']")
+		if( nextPage.length > 0 ) {
+			$( ":mobile-pagecontainer" ).pagecontainer( "change", nextPage, { transition: "none" } );
+		}
+	}
+
+	function previousPage() {
+		var previousPage = $( ":mobile-pagecontainer" ).pagecontainer("getActivePage").prev("div[data-role='page']")
+		if( previousPage.length > 0 ) {
+			$( ":mobile-pagecontainer" ).pagecontainer( "change", previousPage, { transition: "none" } );
+		}
+	}	
+});
